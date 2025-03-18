3add5033c4a306ee0d1c5072e2cfa8c1d9f8e718
Dan Jaheny <danjaheny@mac.home>
2025-03-19 00:51:57 +0100

    evm approval

--- request_builder/construct_request.py
+++ request_builder/construct_request.py
@@ -18,3 +18,27 @@ def construct_request(vault_id, data):
      }
 
     return request_json
+
+def construct_allowance_request(vault_id, evm_chain, proxy_token_contract_address, call_data):
+
+    request_json = {
+
+        "vault_id": vault_id,
+        "signer_type": "api_signer",
+        "type": "evm_transaction",
+        "details": {
+            "type": "evm_raw_transaction",
+            "chain": f"{evm_chain}_mainnet",
+            "gas": {
+                "type": "priority",
+                "priority_level": "medium"
+            },
+            "to": proxy_token_contract_address,
+            "value": "0",
+            "data": {
+                "type": "hex",
+                "hex_data": call_data
+            }
+          }
+    }
+    return request_json
